f0195f691329e4f9700df956eb9421360715031f
BobirDev <91252661+BobirDev@users.noreply.github.com>
2021-11-21 21:57:35 +0500

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,14 +6,13 @@ import "../styles/Navbar.css";
 
 function Navbar() {
   const [openLinks, setOpenLinks] = useState(false);
-
   const toggleNavbar = () => {
     setOpenLinks(!openLinks);
   };
   return (
     <div className="navbar">
       <div className="leftSide" id={openLinks ? "open" : "close"}>
-        <img src={Logo} />
+        <img src={Logo} alt="img"/>
         <div className="hiddenLinks">
           <Link to="/"> Home </Link>
           <Link to="/menu"> Menu </Link>
